311098ec39ee12c67948e95d12bd29ba95d3aeb2
F <kaminoko101@gmail.com>
2022-04-04 21:50:48 +0630

    Remove dark mode for nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav class="bg-white border-gray-200 border-b px-4 sm:px-4 lg:px-8 py-5 rounded dark:bg-gray-800">
+    <nav class="bg-white border-gray-200 border-b px-4 sm:px-4 lg:px-8 py-5 rounded">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="#" class="flex items-center">
-          <span class="text-[#4ADE72] self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span class="text-[#4ADE72] self-center text-2xl font-semibold whitespace-nowrap">
             Freddy Zhao
           </span>
         </a>
         <button
           data-collapse-toggle="mobile-menu"
           type="button"
-          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="mobile-menu"
           aria-expanded="false"
         >
@@ -47,7 +47,7 @@ function NavBar() {
             <li>
               <a
                 href="#about"
-                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition"
+                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 transition"
               >
                 About
               </a>
@@ -55,7 +55,7 @@ function NavBar() {
             <li>
               <a
                 href="#skills"
-                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition"
+                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 transition"
               >
                 Skills
               </a>
@@ -63,7 +63,7 @@ function NavBar() {
             <li>
               <a
                 href="#my-work"
-                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition"
+                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 transition"
               >
                 My Work
               </a>
@@ -71,7 +71,7 @@ function NavBar() {
             <li>
               <a
                 href="#contact"
-                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transition"
+                class="block py-2 pr-4 pl-3 text-lg font-light text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#4ADE72] md:p-0 transition"
               >
                 Contact
               </a>
